2a6a1d99acecec14636fd0aa188a8b799d3b35a7
Cris <hainberg@technikum-wien.at>
2023-07-25 17:40:37 +0200

    Fixed transparent background of edit list
    This should possibly be moved to a main tabulator5 css

--- views/softwareManagement.php
+++ views/softwareManagement.php
@@ -12,7 +12,8 @@
 			//~ 'global' => array('mailAnXversandt'),
 			//~ 'ui' => array('bitteEintragWaehlen')
 		//~ ),
-		'customJSModules' => array('public/extensions/FHC-Core-Softwarebereitstellung/js/apps/SoftwareManagement.js')
+		'customJSModules' => array('public/extensions/FHC-Core-Softwarebereitstellung/js/apps/SoftwareManagement.js'),
+        'customCSSs' => array('public/extensions/FHC-Core-Softwarebereitstellung/css/SoftwareManagement.css')
 	);
 
 	$this->load->view('templates/FHC-Header', $includesArray);
