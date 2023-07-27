5b38167ccc06e7791bf9d4d0e83b7617f4d2c207
Cris <hainberg@technikum-wien.at>
2023-07-27 12:25:33 +0200

    Added method insertSoftwarePlus (insert software and softwarestatus) to Software Model
    Inserts new Software plus new Softwarestatus. Rollback on error.

--- models/Software_model.php
+++ models/Software_model.php
@@ -11,4 +11,45 @@ class Software_model extends DB_Model
 		$this->dbTable = 'extension.tbl_software';
 		$this->pk = 'software_id';
 	}
+
+	/**
+	 * Insert new Software plus new Softwarestatus.
+	 * Rollback on error.
+	 *
+	 * @param $software object
+	 * @param $softwarestatus_kurzbz string
+	 * @return mixed
+	 */
+	public function insertSoftwarePlus($software, $softwarestatus_kurzbz)
+    {
+        // Start DB transaction
+        $this->db->trans_start(false);
+
+        // Insert Software
+        $result = $this->insert($software);
+
+        // Store just inserted software_id
+        $lastInsert_id = getData($result);
+
+        // Insert Softwarestatus
+        $this->load->model('SoftwareSoftwarestatus_Model', 'SoftwareSoftwarestatusModel');
+        $this->SoftwareSoftwarestatusModel->insert(
+            array(
+                'software_id' => $lastInsert_id,
+                'datum' => 'NOW()',
+                'softwarestatus_kurzbz' => $softwarestatus_kurzbz,
+                'uid' => getAuthUID()
+            )
+        );
+
+        // Transaction complete
+        $this->db->trans_complete();
+
+        if ($this->db->trans_status() === false) {
+            $this->db->trans_rollback();
+            return error('Failed inserting Anrechnung', EXIT_ERROR);
+        }
+
+        return success($lastInsert_id);
+    }
 }
