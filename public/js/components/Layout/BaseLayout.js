5c48052d13fee69f56d922bc51bdd4e02fa78180
Cris <hainberg@technikum-wien.at>
2023-08-09 09:52:35 +0200

    Added BaseLayout component
    This component provides a basic layout with
    . header: formatted title and subtitle
    . main and side content: accepts flexible column grid.
    If no columns are set for sidecontent, the main content area will take full width of 12 cols.

---
+++ public/js/components/Layout/BaseLayout.js
@@ -0,0 +1,36 @@
+export const BaseLayout = {
+	props: {
+		coreNav: false,
+		title: '',
+		subtitle: '',
+		mainCols: [],
+		asideCols: [],
+	},
+	computed: {
+		id() {
+			return this.coreNav ? 'content' : '';
+		},
+		mainGridCols() {
+			return this.mainCols.length > 0 ? `col-md-${this.mainCols[0]}` : "col-md-12";
+		},
+
+		asideGridCols() {
+			return this.asideCols.length > 0 ? `col-md-${this.asideCols[0]}` : "";
+		},
+	},
+	template: `
+	<div :id="id" class="overflow-hidden">
+		<header v-if="title">
+			<h1 class="h2 fhc-hr">{{ title }}<span class="fhc-subtitle">{{ subtitle }}</span></h1>
+		</header>
+		<div class="row gx-5">
+			<main :class="mainGridCols">
+				<slot name="main">{{ mainGridCols }}</slot>
+			</main>
+			<aside v-if="asideCols.length > 0" :class="asideGridCols">
+				<slot name="aside">{{ asideGridCols }}</slot>
+			</aside>
+		</div>
+	</div>
+	`
+};
