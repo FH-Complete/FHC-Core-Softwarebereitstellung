3af6e3e2e00d5a1db84980a67df37789df0aad89
Cris <hainberg@technikum-wien.at>
2023-08-10 11:30:05 +0200

    Added Imageverwaltung Table

---
+++ components/filters/ImageVerwaltung.php
@@ -0,0 +1,39 @@
+<?php
+	$filterCmptArray = array(
+		'app' => 'core',
+		'datasetName' => 'imageVerwaltung',
+		'query' => '
+			SELECT
+				si.softwareimage_id, 
+				si.bezeichnung, 
+				si.betriebssystem, 
+				si.verfuegbarkeit_start, 
+				si.verfuegbarkeit_ende, 
+				si.anmerkung, 
+				sio.ort_count,
+				sis.software_count
+			FROM
+				extension.tbl_softwareimage si
+			LEFT JOIN (
+				SELECT
+					softwareimage_id,
+					COUNT(ort_kurzbz) AS ort_count
+				FROM
+					extension.tbl_softwareimage_ort
+				GROUP BY
+					softwareimage_id
+			) sio USING (softwareimage_id)
+			LEFT JOIN (
+				SELECT
+					softwareimage_id,
+					COUNT(software_id) AS software_count
+				FROM
+					extension.tbl_softwareimage_software
+				GROUP BY
+					softwareimage_id
+			) sis USING (softwareimage_id)
+			ORDER BY
+				 bezeichnung
+		',
+		'requiredPermissions' => 'admin'
+	);
--- public/js/components/SoftwareManagement/imageverwaltung/Imageverwaltung.js
+++ public/js/components/SoftwareManagement/imageverwaltung/Imageverwaltung.js
@@ -1,10 +1,98 @@
+import {CoreFilterCmpt} from '../../../../../../js/components/filter/Filter.js';
+import SoftwareModal from "../../Modals/SoftwareModal";
+
 export const Imageverwaltung = {
+	components: {
+		CoreFilterCmpt,
+		SoftwareModal
+	},
+	emits: [
+		'filterMenuUpdated',
+	],
 	data: function() {
 		return {
+			imageVerwaltungTabulatorOptions: { // tabulator options which can be modified after first render
+				maxHeight: "100%",
+				layout: 'fitColumns',
+				index: 'softwareimage_id',
+				columnDefaults:{
+					tooltip:true,
+				},
+				columns: [
+					{title: 'ImageID', field: 'softwareimage_id', visible: false, headerFilter: true, frozen: true},
+					{title: 'Bezeichnung', field: 'bezeichnung', headerFilter: true, frozen: true},
+					{title: 'Betriebssystem', field: 'betriebssystem', headerFilter: true},
+					{title: 'Verfügbarkeit Start', field: 'verfuegbarkeit_start', headerFilter: true},
+					{title: 'Verfügbarkeit Ende', field: 'verfuegbarkeit_ende', headerFilter: true},
+					{title: 'Anzahl Räume', field: 'ort_count', headerFilter: true, hozAlign: 'right'},
+					{title: 'Anzahl Software', field: 'software_count', headerFilter: true, hozAlign: 'right'},
+					{title: 'Anmerkung', field: 'anmerkung', headerFilter: true},
+					{
+						title: 'Aktionen',
+						field: 'actions',
+						hozAlign: 'center',
+						formatter: (cell, formatterParams, onRendered) => {
+							let container = document.createElement('div');
+							container.className = "d-flex gap-2";
 
+							let button = document.createElement('button');
+							button.className = 'btn btn-outline-secondary';
+							button.innerHTML = '<i class="fa fa-edit"></i>';
+							button.addEventListener('click', (event) => this.editSoftwareimage(event, cell.getRow().getIndex()));
+							container.append(button);
+
+							button = document.createElement('button');
+							button.className = 'btn btn-outline-secondary';
+							button.innerHTML = '<i class="fa fa-xmark"></i>';
+							button.addEventListener('click', () => this.deleteSoftwareimage(cell.getRow().getIndex()));
+							container.append(button);
+
+							return container;
+						}
+					}
+				]
+			},
+			tabulatorAdditionalColumns: ['actions']
 		}
 	},
+	methods: {
+		openModal(event) {
+			console.log('* openModal');
+			console.log(event);
+		},
+		editSoftwareimage(event, softwareimage_id){
+			console.log('* editSoftwareimage');
+			console.log(softwareimage_id);
+		},
+		deleteSoftwareimage(softwareimage_id) {
+			console.log('* deleteSoftwareimage');
+			console.log(softwareimage_id);
+		},
+		updateFilterMenuEntries: function(payload) {
+			console.log('* IMG / updateFilterMenuEntries: payload.children:');
+			console.log(payload.children[0]);
+			this.$emit('filterMenuUpdated', payload);
+		},
+		reloadTabulator() {
+			for (let option in this.imageVerwaltungTabulatorOptions)
+			{
+				if (this.$refs.imageVerwaltungTable.tabulator.options.hasOwnProperty(option))
+					this.$refs.imageVerwaltungTable.tabulator.options[option] = this.imageVerwaltungTabulatorOptions[option];
+			}
+			this.$refs.imageVerwaltungTable.reloadTable();
+		},
+	},
 	template: `
-	<h3>Imageverwaltung Content</h3>
+	<!-- Imageverwaltung Tabelle -->
+	<core-filter-cmpt
+		ref="imageVerwaltungTable"
+		filter-type="ImageVerwaltung"
+		:tabulator-options="imageVerwaltungTabulatorOptions"
+		:tabulatorAdditionalColumns="tabulatorAdditionalColumns"
+		:new-btn-label="'Image'"
+		:new-btn-show="true"
+		@nw-new-entry="updateFilterMenuEntries"
+		@click:new="openModal">
+	</core-filter-cmpt>
 	`
 };
