3913fa2a5f61fe66e4a5faa35b4e4b5aff5024fa
Cris <hainberg@technikum-wien.at>
2023-08-22 16:12:33 +0200

    Add new method updateSoftwareimageAndOrte to Softwareimage Model
    . Update Softwareimage, add new Räume assigned to Softwareimage and delete Räume that were removed.
    . Rollback on error.

--- models/Softwareimage_model.php
+++ models/Softwareimage_model.php
@@ -55,4 +55,71 @@ class Softwareimage_model extends DB_Model
 		return success($lastInsert_id);
 	}
 
+	/**
+	 * Update Softwareimage, add new Räume assigned to Softwareimage and delete Räume that were removed.
+	 * Rollback on error.
+	 *
+	 * @param $softwareimage array
+	 * @param $orte_kurzbz array
+	 * @return mixed
+	 */
+	public function updateSoftwareimageAndOrte($softwareimage, $orte_kurzbz)
+	{
+		// Start DB transaction
+		$this->db->trans_start(false);
+
+		// Update Softwareimage
+		$this->update($softwareimage['softwareimage_id'], $softwareimage);
+
+		// Add new Räume assigned to Softwareimage and delete Räume that were removed
+		if (isset($orte_kurzbz) && is_array($orte_kurzbz))
+		{
+			$this->load->model('extensions/FHC-Core-Softwarebereitstellung/SoftwareimageOrt_model', 'SoftwareimageOrtModel');
+
+			// Step 1: Add new Räume
+			foreach ($orte_kurzbz as $ort_kurzbz) {
+				$qry = "
+					INSERT INTO extension.tbl_softwareimage_ort (softwareimage_id, ort_kurzbz) 
+					SELECT ?, ?
+					WHERE ? NOT IN  (SELECT ort_kurzbz FROM extension.tbl_softwareimage_ort WHERE softwareimage_id = ?);
+				";
+
+				$this->SoftwareimageOrtModel->execQuery($qry,
+					array(
+						$softwareimage['softwareimage_id'],
+						$ort_kurzbz,
+						$ort_kurzbz,
+						$softwareimage['softwareimage_id']
+					)
+				);
+			}
+
+			// Step 2: Delete Räume
+			$qry = "
+					DELETE FROM extension.tbl_softwareimage_ort
+					WHERE softwareimage_id = ? 
+					  AND ort_kurzbz NOT IN ?;
+				";
+
+			$this->SoftwareimageOrtModel->execQuery($qry,
+				array(
+					$softwareimage['softwareimage_id'],
+					$orte_kurzbz,
+				)
+			);
+		}
+
+		// Transaction complete
+		$this->db->trans_complete();
+
+		if ($this->db->trans_status() === false)
+		{
+			$this->db->trans_rollback();
+			return error('Fehler beim Update des Softwareimages', EXIT_ERROR);
+		}
+
+		return success();
+	}
+
+
 }
