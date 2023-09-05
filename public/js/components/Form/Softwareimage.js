f193ccb719d343d97be3f04f98576fd5d750a4c7
Cris <hainberg@technikum-wien.at>
2023-09-05 11:02:23 +0200

    Removed Dropdown to assign Räume from Add-Image-Form
    . also adapted to only insert/update Image without Raumzuordnung
    . removed unnecessary ids from template

--- public/js/components/Form/Softwareimage.js
+++ public/js/components/Form/Softwareimage.js
@@ -11,8 +11,6 @@ export const Softwareimage = {
 		return {
 			softwareimageId: null,
 			softwareimage: {},
-			orte: [],
-			ortSuggestions: [],
 			errors: []
 		}
 	},
@@ -45,30 +43,6 @@ export const Softwareimage = {
 					}
 				);
 
-				// Get Räume assigned to softwareimage
-				CoreRESTClient.get('/extensions/FHC-Core-Softwarebereitstellung/components/Ort/getOrteByImage',
-					{
-						softwareimage_id: softwareimage_id
-					}
-				).then(
-					result => {
-						if (CoreRESTClient.isError(result.data)) {
-							this.errors.push(result.data.retval);
-						}
-						else {
-							if (CoreRESTClient.hasData(result.data)) {
-								// Prefill form with Räume assigned to softwareimage
-								this.orte = CoreRESTClient.getData(result.data);
-							}
-						}
-					}
-				).catch(
-					error => {
-						let errorMessage = error.message ? error.message : 'Unknown error';
-						alert('Error when getting orte assigned to softwareimage: ' + errorMessage);
-					}
-				);
-
 			}
 		},
 		save(){
@@ -81,13 +55,12 @@ export const Softwareimage = {
 			}
 
 			// Decide if create or update image
-			let method = Number.isInteger(this.softwareimageId) ? 'updateImageAndOrte' : 'createImageAndOrte';
+			let method = Number.isInteger(this.softwareimageId) ? 'updateImage' : 'createImage';
 
 			CoreRESTClient.post(
 				'/extensions/FHC-Core-Softwarebereitstellung/components/Image/' + method,
 				{
 					softwareimage: this.softwareimage,
-					orte_kurzbz: this.orte.map(ort => ort.ort_kurzbz)
 				}
 			).then(
 				result => {
@@ -115,7 +88,6 @@ export const Softwareimage = {
 		reset(){
 			this.softwareimageId = null,
 			this.softwareimage = {},
-			this.orte = [];
 			this.errors = [];
 			
 		},
@@ -149,31 +121,17 @@ export const Softwareimage = {
 		<form ref="softwareimageForm" class="row">
 			<div class="col-sm-9 mb-6">
 				<label :for="bezeichnung" class="form-label">Bezeichnung *</label>
-				<input type="text" class="form-control mb-3" :id="bezeichnung" v-model="softwareimage.bezeichnung" required >
+				<input type="text" class="form-control mb-3" v-model="softwareimage.bezeichnung" required >
 				<label :for="betriebssystem" class="form-label">Betriebssystem</label>
-				<input type="text" class="form-control mb-3" :id="betriebssystem"  v-model="softwareimage.betriebssystem">	
+				<input type="text" class="form-control mb-3" v-model="softwareimage.betriebssystem">	
 				<label :for="verfuegbarkeit_start" class="form-label">Verfügbarkeit Start</label>
-				<input type="date" class="form-control mb-3" :id="verfuegbarkeit_start"  v-model="softwareimage.verfuegbarkeit_start">
+				<input type="date" class="form-control mb-3" v-model="softwareimage.verfuegbarkeit_start">
 				<label :for="verfuegbarkeit_ende" class="form-label">Verfügbarkeit Ende</label>
-				<input type="date" class="form-control mb-3" :id="verfuegbarkeit_ende"  v-model="softwareimage.verfuegbarkeit_ende">
-				<label :for="softwareimage_ort" class="form-label">Raumzuordnung</label>
-				<auto-complete
-					inputId="softwareimage_ort"
-					class="w-100 mb-3"
-					v-model="orte"
-					optionLabel="ort_kurzbz"
-					dropdown
-					dropdown-current
-					forceSelection
-					multiple
-					:suggestions="ortSuggestions"
-					@complete="onComplete">
-				</auto-complete>
+				<input type="date" class="form-control mb-3" v-model="softwareimage.verfuegbarkeit_ende">
 				<label :for="anmerkung" class="form-label">Anmerkung</label>
 				<textarea
 					class="form-control mb-3"
 					v-model="softwareimage.anmerkung"
-					:id="anmerkung"
 					rows="5">
 				</textarea>
 			</div>
