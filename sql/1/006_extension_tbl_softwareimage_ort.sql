25cccb26ae1ea48301631ff6eef8653b7136ca5b
Cris <hainberg@technikum-wien.at>
2023-09-05 09:48:02 +0200

    Adapted DB query to use primary key softwareimageort_id instead of composite primary key

--- sql/1/006_extension_tbl_softwareimage_ort.sql
+++ sql/1/006_extension_tbl_softwareimage_ort.sql
@@ -1,13 +1,23 @@
 CREATE TABLE IF NOT EXISTS extension.tbl_softwareimage_ort (
+    softwareimageort_id integer NOT NULL,
 	softwareimage_id integer NOT NULL,
 	ort_kurzbz varchar(32) NOT NULL,
 	verfuegbarkeit_start date,
 	verfuegbarkeit_ende date,
-	CONSTRAINT tbl_softwareimage_ort_pk PRIMARY KEY (softwareimage_id, ort_kurzbz)
+	CONSTRAINT tbl_softwareimage_ort_pk PRIMARY KEY (softwareimageort_id)
 );
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE extension.tbl_softwareimage_ort TO vilesci;
 GRANT SELECT ON TABLE extension.tbl_softwareimage_ort TO web;
+
+CREATE SEQUENCE IF NOT EXISTS extension.tbl_softwareimage_ort_softwareimageort_id_seq
+	 INCREMENT BY 1
+	 NO MAXVALUE
+	 NO MINVALUE
+	 CACHE 1;
+ALTER TABLE extension.tbl_softwareimage_ort ALTER COLUMN softwareimageort_id SET DEFAULT nextval('extension.tbl_softwareimage_ort_softwareimageort_id_seq');
+
+GRANT SELECT, UPDATE ON extension.tbl_softwareimage_ort_softwareimageort_id_seq TO vilesci;
 
 DO $$
 BEGIN
