b5b41bb1161c5ddd715cdde748918392aa4e5ea3
Cris <hainberg@technikum-wien.at>
2024-04-03 11:44:10 +0200

    Fixed Softwarestatus-Dropdown selected status to display bezeichnung instead of Kurzbz

--- public/js/components/SoftwareManagement/softwareverwaltung/Actions.js
+++ public/js/components/SoftwareManagement/softwareverwaltung/Actions.js
@@ -15,9 +15,9 @@ export const Actions = {
 		}
 	},
 	methods: {
-		changeStatus(softwarestatus_kurzbz) {
-			this.selected = softwarestatus_kurzbz;
-			this.$emit("setStatus", softwarestatus_kurzbz);
+		changeStatus(softwarestatus) {
+			this.selected = softwarestatus.bezeichnung;
+			this.$emit("setStatus", softwarestatus.softwarestatus_kurzbz);
 		},
 
 		handleHierarchyViewChange (event) {
@@ -34,7 +34,7 @@ export const Actions = {
 			</button>
 			 <ul class="dropdown-menu" aria-labelledby="statusDropdown">
 				<li v-for="status in softwarestatus" :key="status.softwarestatus_kurzbz">
-					<a class="dropdown-item" @click="changeStatus(status.softwarestatus_kurzbz)">{{status.bezeichnung}}</a>
+					<a class="dropdown-item" @click="changeStatus(status)">{{status.bezeichnung}}</a>
 				</li>
 			</ul>
 		</div>
