58e83920450bcb4eee3a8dd0541740580b828b34
Cris <hainberg@technikum-wien.at>
2024-05-28 11:45:59 +0200

    Added getSoftwarelistData to Software_model.php to get initial table data

--- models/Software_model.php
+++ models/Software_model.php
@@ -14,6 +14,64 @@ class Software_model extends DB_Model
 	}
 
 	/**
+	 * Get initial table data.
+	 * @return mixed
+	 */
+	public function getSoftwarelistData()
+	{
+		$query= "
+			SELECT * FROM (
+				SELECT
+					DISTINCT ON (sw.software_id)
+					sw.software_id,
+					sw.software_kurzbz,
+					sw.softwaretyp_kurzbz,
+					sw.version,
+					sw.beschreibung,
+					sw.hersteller,
+					sw.os,
+					sw.verantwortliche,
+					sw_status.softwarestatus_kurzbz,
+					sw.anmerkung_intern,
+					sw.software_id_parent,
+					sw_parent.software_kurzbz AS software_kurzbz_parent,
+					sw_typ.bezeichnung[(" . $this->_getLanguageIndex() . ")] AS softwaretyp_bezeichnung,
+					sw_status.bezeichnung[(" . $this->_getLanguageIndex() . ")] AS softwarestatus_bezeichnung,
+					sw.insertamum::date,
+					(
+						SELECT
+							CONCAT(person.vorname, ' ', person.nachname)
+						FROM
+							tbl_person AS person
+							JOIN tbl_benutzer AS benutzer USING (person_id)
+						WHERE
+							benutzer.uid = sw.insertvon
+					) AS insertvon,
+					sw.updateamum::date,
+					(
+						SELECT
+							CONCAT(person.vorname, ' ', person.nachname)
+						FROM
+							tbl_person AS person
+							JOIN tbl_benutzer AS benutzer USING (person_id)
+						WHERE
+							benutzer.uid = sw.updatevon
+					) AS updatevon
+				FROM
+					extension.tbl_software sw
+					JOIN extension.tbl_softwaretyp sw_typ USING (softwaretyp_kurzbz)
+					LEFT JOIN extension.tbl_software_softwarestatus sw_swstatus USING (software_id)
+					LEFT JOIN extension.tbl_softwarestatus sw_status USING (softwarestatus_kurzbz)
+					LEFT JOIN extension.tbl_software sw_parent ON sw.software_id_parent = sw_parent.software_id
+				ORDER BY
+					sw.software_id DESC, sw_swstatus.datum DESC, sw_swstatus.software_status_id DESC
+			) software
+			ORDER BY software_kurzbz, version DESC NULLS LAST, software_id DESC";
+
+		return $this->execQuery($query);
+	}
+
+	/**
 	 * Insert new Software plus new Softwarestatus.
 	 * Rollback on error.
 	 *
