d69a89c51a0894b80ffe7e5063fe32e716c101a0
Cris <hainberg@technikum-wien.at>
2024-05-28 11:44:55 +0200

    Added berechtigung 'softwareliste' to tbl_berechtigung to access Softwareliste

--- sql/1/000_extension.sql
+++ sql/1/000_extension.sql
@@ -1,6 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS extension;
 
 INSERT INTO system.tbl_berechtigung(berechtigung_kurzbz, beschreibung)
+SELECT 'extension/softwareliste', 'Softwareliste als Info für die Lehre'
+    WHERE
+	NOT EXISTS(SELECT 1 FROM system.tbl_berechtigung WHERE berechtigung_kurzbz='extension/softwareliste');
+
+INSERT INTO system.tbl_berechtigung(berechtigung_kurzbz, beschreibung)
 SELECT 'extension/software_verwalten', 'Software- und Lizenzmanagement'
     WHERE
 	NOT EXISTS(SELECT 1 FROM system.tbl_berechtigung WHERE berechtigung_kurzbz='extension/software_verwalten');
