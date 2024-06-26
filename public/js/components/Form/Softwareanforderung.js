614d1bb260846554d3f36713120b2d9735f4f8d1
Cris <hainberg@technikum-wien.at>
2024-06-26 09:54:10 +0200

    Created Component and Form to select SW from Table and assign to Lehrveranstaltungen

---
+++ public/js/components/Form/Softwareanforderung.js
@@ -0,0 +1,360 @@
+import CoreForm from "../../../../../js/components/Form/Form.js";
+import CoreFormInput from "../../../../../js/components/Form/Input.js";
+import CoreFormValidation from "../../../../../js/components/Form/Validation.js";
+import CoreBsModal from '../../../../../js/components/Bootstrap/Modal.js';
+
+export default {
+	components: {
+		CoreForm,
+		CoreFormInput,
+		CoreFormValidation,
+		CoreBsModal
+	},
+	data() {
+		return {
+			modalTitel: '',
+			autocompleteAbortController: null,
+			lvSuggestions: [],
+			swSuggestions: [],
+			studiensemester: [],
+			selectedStudiensemester: '',
+			selectedLvs: [],
+			selectedSw: [],
+			formData: [],
+			requestModus: '' // 'sw' if request by software, 'lv' if request by lv
+		};
+	},
+	watch: {
+		selectedSw(){
+			this.selectedLvs.length > 0 && this.selectedSw.length > 0
+				? this.generateSwLvRows()
+				: this.isLvSwRowsVisible = false;
+
+		},
+		selectedLvs(){
+			this.selectedLvs.length > 0 && this.selectedSw.length > 0
+				? this.generateSwLvRows()
+				: this.isLvSwRowsVisible = false;
+		}
+	},
+	emit: ['formClosed'],
+	methods: {
+		sendForm() {
+			// Reduce postData for backend-needs
+			const postData = this.formData
+				.filter(item => !item.zuordnungExists)	// Keep only new SW-LV-Zuordnungen, that do not already exist
+				.map(({ software_id, lehrveranstaltung_id, studiensemester_kurzbz, lizenzanzahl }) => ({
+					software_id,
+					lehrveranstaltung_id,
+					studiensemester_kurzbz,
+					lizenzanzahl
+			}));
+
+			// Return if no new SW-LV-Zuordnungen found
+			if (postData.length === 0){
+				this.$fhcAlert.alertWarning(this.$p.t('global', 'swWurdeBereitsAngefordert'));
+				return;
+			}
+
+			// Save SW-LV-Zuordnungen
+			if (this.$refs.form)
+				this.$refs.form
+					.post('extensions/FHC-Core-Softwarebereitstellung/fhcapi/Softwareanforderung/saveSoftwareLv', postData)
+					.then(result => {
+						this.$fhcAlert.alertSuccess(this.$p.t('ui', 'gespeichert'));
+						this.requestModus == 'sw' ? this.resetFormButKeepSelectedSw() : this.resetFormButKeepSelectedLv();
+					})
+					.catch(this.$fhcAlert.handleSystemError);
+		},
+		openModalSwToLv(selectedData) {
+			this.requestModus = 'sw';
+
+			// Reset form
+			this.resetForm();
+
+			// Prefill software select with data of table selection
+			if (Array.isArray(selectedData)) {
+				this.selectedSw = selectedData.map(data => ({
+					'software_id': data.software_id,
+					'software_kurzbz': data.software_kurzbz
+				}));
+			}
+
+			// Set modal title
+			this.modalTitel = this.$p.t('global', 'swFuerLvAnfordern');
+
+			// Load studiensemester dropdown
+			this.loadStudiensemesterDropdown();
+
+			// Open modal
+			this.$refs.modalContainer.show();
+		},
+		loadStudiensemesterDropdown(){
+			this.$fhcApi
+				.get('extensions/FHC-Core-Softwarebereitstellung/fhcapi/Softwareanforderung/getAktAndFutureSemester')
+				.then( result => {
+					this.studiensemester = result.data;
+					this.selectedStudiensemester = this.studiensemester.length > 0 ? this.studiensemester[0].studiensemester_kurzbz : '';
+				})
+				.catch( this.$fhcAlert.handleSystemError );
+		},
+		onStudiensemesterChange(event){
+			// Empty lehrveranstaltungen dropdown
+			this.selectedLvs = [];
+
+			// Hide section with SW-LV-Zuordnungen
+			this.isLvSwRowsVisible = false;
+		},
+		searchSw(event) {
+			if (event.query || !this.swSuggestions.length) {
+				if (this.autocompleteAbortController)
+					this.autocompleteAbortController.abort();
+				this.autocompleteAbortController = new AbortController();
+
+				this.$fhcApi
+					.get(
+						'extensions/FHC-Core-Softwarebereitstellung/fhcapi/Softwareanforderung/autocompleteSwSuggestions/' + encodeURIComponent(event.query),
+						null,
+						{
+							signal: this.autocompleteAbortController.signal
+						}
+					)
+					.then(result => {this.swSuggestions = result.data})
+					.catch(this.$fhcAlert.handleSystemError);
+			}
+		},
+		searchLv(event) {
+			if (event.query || !this.lvSuggestions.length) {
+				if (this.autocompleteAbortController)
+					this.autocompleteAbortController.abort();
+				this.autocompleteAbortController = new AbortController();
+				this.$fhcApi
+					.get(
+						'extensions/FHC-Core-Softwarebereitstellung/fhcapi/Softwareanforderung/autocompleteLvSuggestionsByStudsem/' + encodeURIComponent(event.query),
+						{
+							studiensemester_kurzbz: this.selectedStudiensemester
+						},
+						{
+							signal: this.autocompleteAbortController.signal,
+						}
+					)
+					.then(result => {
+						let data = result.data;
+						let groupedData = {};
+						data.forEach(item => {
+							const key = item.lv_oe_bezeichnung;
+
+							if (!groupedData[key]) {
+								groupedData[key] = {
+									stg_bezeichnung: item.stg_bezeichnung,
+									lvs: []
+								};
+							}
+
+							const lv = {
+								lehrveranstaltung_id: item.lehrveranstaltung_id,
+								lv_bezeichnung: item.lv_bezeichnung,
+								lv_oe_kurzbz: item.lv_oe_kurzbz,
+								lv_oe_bezeichnung: item.lv_oe_bezeichnung
+							};
+
+							groupedData[key].lvs.push(lv);
+
+						})
+						this.lvSuggestions = Object.values(groupedData);
+					})
+					.catch(this.$fhcAlert.handleSystemError);
+			}
+		},
+		isLvSelected(option){
+			// Disable option if lv is selected
+			return this.selectedLvs.some(item => item.lehrveranstaltung_id === option.lehrveranstaltung_id);
+		},
+		isSwSelected(option){
+			// Disable option if sw is selected
+			return this.selectedSw.some(item => item.software_kurzbz === option.software_kurzbz);
+		},
+		resetForm(){
+			this.formData = [];
+			this.selectedStudiensemester = this.studiensemester.length > 0 ? this.studiensemester[0].studiensemester_kurzbz : '';
+			this.selectedLvs = [];
+			this.selectedSw = [];
+			this.isLvSwRowsVisible = false;
+		},
+		resetFormButKeepSelectedSw(){
+			this.formData = [];
+			this.selectedStudiensemester = this.studiensemester.length > 0 ? this.studiensemester[0].studiensemester_kurzbz : '';
+			this.selectedLvs = [];
+			this.isLvSwRowsVisible = false;
+		},
+		resetFormButKeepSelectedLv(){
+			this.formData = [];
+			this.selectedStudiensemester = this.studiensemester.length > 0 ? this.studiensemester[0].studiensemester_kurzbz : '';
+			this.selectedSw = [];
+			this.isLvSwRowsVisible = false;
+		},
+		generateSwLvRows(){
+			// Reset formData
+			this.formData = [];
+
+			// Push selected data to formData
+			for(const lv of this.selectedLvs){
+				for (const sw of this.selectedSw){
+					this.formData.push({
+						studiensemester_kurzbz: this.selectedStudiensemester,
+						lv_oe_bezeichnung: lv.lv_oe_bezeichnung,
+						lehrveranstaltung_id: lv.lehrveranstaltung_id,
+						lv_bezeichnung: lv.lv_bezeichnung,
+						software_id: sw.software_id,
+						software_kurzbz: sw.software_kurzbz,
+						lizenzanzahl: 0,  // Default
+						zuordnungExists: false // Default
+					})
+				}
+			}
+
+			// Sort formData
+			this.formData.sort((a, b) =>
+				a.lv_oe_bezeichnung.localeCompare(b.lv_oe_bezeichnung) ||
+				a.lv_bezeichnung.localeCompare(b.lv_bezeichnung)
+			);
+
+			// Flag if selection already exists
+			this.flagExistingSwLvZuordnungen(this.formData);
+
+			// Display formData as rows
+			this.isLvSwRowsVisible = true;
+
+		},
+		flagExistingSwLvZuordnungen(){
+			this.$fhcApi
+				.post('extensions/FHC-Core-Softwarebereitstellung/fhcapi/Softwareanforderung/checkAndGetExistingSwLvZuordnungen', this.formData)
+				.then( result => {
+					if (result.data.length > 0)
+					{
+						this.formData.forEach(formItem => {
+							result.data.forEach(existingItem => {
+								// Flag formData where SW-LV Zuordnung already exists
+								if (
+									formItem.software_id === existingItem.software_id &&
+									formItem.lehrveranstaltung_id === existingItem.lehrveranstaltung_id &&
+									formItem.studiensemester_kurzbz === existingItem.studiensemester_kurzbz
+								) {
+									formItem.lizenzanzahl = existingItem.lizenzanzahl
+									formItem.zuordnungExists = true;
+								}
+							});
+						});
+					}
+				})
+				.catch( this.$fhcAlert.handleSystemError );
+		}
+	},
+	template: `
+	<div class="app-example-form-1">
+		<core-form ref="form" @submit.prevent="sendForm">
+			<core-bs-modal ref="modalContainer" class="bootstrap-prompt" dialog-class="modal-xl" @hidden-bs-modal="$emit('formClosed')">
+				<template #title>{{ modalTitel }}</template>
+				<template #default>
+					<!-- Formular -->
+					<core-form-validation></core-form-validation>
+					<div class="row">
+						<div class="col-2 mb-3">
+							<core-form-input
+								type="select"
+								v-model="selectedStudiensemester"
+								name="studiensemester"
+								:label="$p.t('lehre', 'studiensemester')"
+								@change="onStudiensemesterChange">
+								<option 
+								v-for="(studSem, index) in studiensemester"
+								:key="index" 
+								:value="studSem.studiensemester_kurzbz">
+									{{studSem.studiensemester_kurzbz}}
+								</option>
+							</core-form-input>
+						</div>
+						<div class="col-10 mb-3">
+							<core-form-input
+								type="autocomplete"
+								v-model="selectedLvs"
+								name="selectedLvs"
+								label="Lehrveranstaltungen *"
+								option-group-label="stg_bezeichnung"
+								option-group-children="lvs"
+								option-label="lv_bezeichnung"
+								:option-disabled="isLvSelected"
+								dropdown
+								multiple
+								:suggestions="lvSuggestions"
+								@complete="searchLv">
+							</core-form-input>
+						</div>
+						<div class="col-12 mb-3">
+							<core-form-input
+								type="autocomplete"
+								v-model="selectedSw"
+								name="selectedSw"
+								label="Software *"
+								option-label="software_kurzbz"
+								:option-disabled="isSwSelected"
+								dropdown
+								multiple
+								@complete="searchSw"
+								:suggestions="swSuggestions">
+							</core-form-input>
+						</div>
+					</div>
+					<div class="fhc-hr mt-n-3" v-if="isLvSwRowsVisible"></div>
+					<div class="row" v-if="isLvSwRowsVisible" v-for="(fd, index) in formData" :key="index">
+							<div class="col-2 mb-2">
+								<core-form-input
+									v-model="fd.lv_oe_bezeichnung"
+									name="lv_oe_bezeichnung"
+									:label="$p.t('lehre', 'organisationseinheit')"
+									class="form-control-sm"
+									readonly>
+								</core-form-input>
+							</div>
+							<div class="col-4 mb-2">
+								<core-form-input
+									v-model="fd.lv_bezeichnung"
+									name="lv_bezeichnung"
+									:label="$p.t('lehre', 'lehrveranstaltung')"
+									class="form-control-sm"
+									readonly>
+								</core-form-input>
+							</div>
+							<div class="col-4 mb-2">
+								<core-form-input
+									v-model="fd.software_kurzbz"
+									name="software_kurzbz"
+									label="Software"
+									class="form-control-sm"
+									readonly>
+								</core-form-input>
+							</div>
+							<div class="col-2 mb-2">
+							<core-form-input
+								type="number"
+								v-model="fd.lizenzanzahl"
+								name="lizenzanzahl"
+								class="form-control-sm"
+								:label="$p.t('global', 'lizenzAnzahl')"
+								:disabled="fd.zuordnungExists">
+							</core-form-input>
+							<div class="form-text text-danger" v-if="fd.zuordnungExists">{{ $p.t('global/bereitsAngefordert') }}</div>
+						</div>
+					</div>
+				</template>
+				<template #footer>
+					<button type="button" class="btn btn-primary" 
+						:disabled="!isLvSwRowsVisible" 
+						@click="sendForm">
+						{{ $p.t('global', 'swFuerLvAnfordern') }}
+					</button>
+				</template>
+			</core-bs-modal>
+		</core-form>
+	</div>`
+}
---
+++ public/js/components/Softwareanforderung/SoftwareanforderungNachSw.js
@@ -0,0 +1,244 @@
+import {CoreFilterCmpt} from '../../../../../js/components/filter/Filter.js';
+import {CoreRESTClient} from '../../../../../js/RESTClient.js';
+import {Raumzuordnung} from "../SoftwareManagement/Raumzuordnung.js";
+import SoftwareanforderungForm from "../Form/Softwareanforderung.js";
+
+// Fields used to restructure table data for dataTree
+const idField = 'software_id';
+const parentIdField = 'software_id_parent';
+
+export default {
+	components: {
+		CoreFilterCmpt,
+		SoftwareanforderungForm,
+		Raumzuordnung
+	},
+	data: function() {
+		return {
+			data: {},
+			cbDataTree: true, // checkbox display dataTree or not
+			cbDataTreeStartExpanded: false,	// checkbox expand dataTree or not
+		}
+	},
+	computed: {
+		tabulatorOptions() {
+			const self = this;
+			return {
+				ajaxURL: CoreRESTClient._generateRouterURI('extensions/FHC-Core-Softwarebereitstellung/components/Software/getSoftwarelistData'),
+				ajaxResponse(url, params, response){
+					// If dataTree checkbox is checked
+					return self.cbDataTree
+						? self.prepDataTreeData(CoreRESTClient.getData(response)) // Prepare data for dataTree view
+						: CoreRESTClient.getData(response); // else return data for normal list view
+				},
+				dataTree: self.cbDataTree,
+				dataTreeStartExpanded: self.cbDataTreeStartExpanded,
+				layout: 'fitColumns',
+				index: 'software_id',
+				columns: [
+					{
+						formatter: 'rowSelection',
+						titleFormatter: 'rowSelection',
+						titleFormatterParams: { rowRange: "active"},
+						width: 70,
+						frozen: true
+					},
+					{title: 'ID', field: 'software_id', headerFilter: true, visible: false, frozen: true},
+					{title: 'Software', field: 'software_kurzbz', headerFilter: true,
+						frozen: true,
+						width: 200,
+						minWidth: 200,
+						maxWidth: 200,},
+					{title: 'Version', field: 'version', headerFilter: true, hozAlign: 'right'},
+					{title: this.$p.t('global/softwaretyp'), field: 'softwaretyp_bezeichnung', headerFilter: true,
+						formatter: (cell) => {
+							return cell.getValue();
+						}
+					},
+					{title: this.$p.t('global/softwaretypKurzbz'), field: 'softwaretyp_kurzbz', headerFilter: true, visible: false},
+					{title: this.$p.t('global/hersteller'), field: 'hersteller', headerFilter: true},
+					{title: this.$p.t('global/betriebssystem'), field: 'os', headerFilter: true},
+					{title: this.$p.t('global/beschreibung'), field: 'beschreibung', headerFilter: true, visible: false},
+					{title: this.$p.t('global/verantwortliche'), field: 'verantwortliche', headerFilter: true, visible: false},
+					{title: this.$p.t('global/anmerkungIntern'), field: 'anmerkung_intern', headerFilter: true, visible: false},
+					{title: 'Übergeordnete Software ID', field: 'software_id_parent', headerFilter: true, visible: false},
+					{title: this.$p.t('global/uebergeordneteSoftware'), field: 'software_kurzbz_parent', headerFilter: true, visible: false},
+					{title: this.$p.t('global/insertamum'), field: 'insertamum', hozAlign:"center", headerFilter: true, visible: false},
+					{title: this.$p.t('global/insertvon'), field: 'insertvon', headerFilter: true, visible: false},
+					{title: this.$p.t('global/updateamum'), field: 'updateamum', hozAlign:"center", headerFilter: true, visible: false},
+					{title: this.$p.t('global/updatevon'), field: 'updatevon', headerFilter: true, visible: false},
+					{title: 'Software-Status', field: 'softwarestatus_kurzbz', headerFilter: true,
+						formatter: (cell) => this.softwarestatus
+							? this.softwarestatus[cell.getValue()]
+							: cell.getData().softwarestatus_bezeichnung,
+						width: 150,
+						minWidth: 150,
+						maxWidth: 150,
+						frozen: true
+					},
+					{title: this.$p.t('global/aktionen'), field: 'actions',
+						width: 380,
+						minWidth: 380,
+						maxWidth: 380,
+						formatter: (cell, formatterParams, onRendered) => {
+							let container = document.createElement('div');
+							container.className = "d-flex gap-2";
+
+							let button = document.createElement('button');
+							button.className = 'btn btn-outline-secondary';
+							button.innerHTML = this.$p.t('global/raumverfuegbarkeit');
+							button.addEventListener(
+								'click', (event) => this.openRaumzuordnung(event, cell.getRow())
+							);
+							container.append(button);
+
+							return container;
+						},
+						frozen: true
+					}
+				]
+			}
+		}
+	},
+	methods: {
+		openRaumzuordnung(e, row){
+			// Get Orte for a software
+			this.$refs.raumzuordnung.getOrteBySoftware(row.getIndex(), row.getData().software_kurzbz);
+
+			// Open Details in offcanvas
+			let offcanvasElement = new bootstrap.Offcanvas(document.getElementById('softwarelisteOffcanvas'));
+			offcanvasElement.show();
+		},
+		openSoftwareanforderungForm(){
+			let selectedData = this.$refs.softwareanforderungNachSwTable.tabulator.getSelectedData();
+
+			if (selectedData.length == 0)
+			{
+				this.$fhcAlert.alertWarning( this.$p.t('global/zeilenAuswaehlen'));
+				return;
+			}
+
+			this.$refs.softwareanforderungForm.openModalSwToLv(selectedData);
+		},
+		reloadTabulator() {
+			if (this.$refs.softwareanforderungNachSwTable.tabulator !== null && this.$refs.softwareanforderungNachSwTable.tabulator !== undefined)
+			{
+				for (let option in this.tabulatorOptions)
+				{
+					if (this.$refs.softwareanforderungNachSwTable.tabulator.options.hasOwnProperty(option))
+						this.$refs.softwareanforderungNachSwTable.tabulator.options[option] = this.tabulatorOptions[option];
+				}
+				this.$refs.softwareanforderungNachSwTable.reloadTable();
+			}
+		},
+		prepDataTreeData(data){
+			let toDelete = [];
+
+			// loop through all data
+			for (let childIdx = 0; childIdx < data.length; childIdx++)
+			{
+				let child = data[childIdx];
+
+				// if it has parent id, it is a child
+				if (child[parentIdField])
+				{
+					// append the child on the right place. If parent found, mark original sw child on 0 level for deleting
+					if (this._appendChild(data, child)) toDelete.push(childIdx);
+				}
+			}
+
+			// delete the marked children from 0 level
+			for (let counter = 0; counter < toDelete.length; counter++)
+			{
+				// decrease index by counter as index of data array changes after every deletion
+				data.splice(toDelete[counter] - counter, 1);
+			}
+
+			return data;
+		},
+		_appendChild(data, child) {
+			// get parent id
+			let parentId = child[parentIdField];
+
+			// loop thorugh all data
+			for (let parentIdx = 0; parentIdx < data.length; parentIdx++)
+			{
+				let parent = data[parentIdx];
+
+				// if it's the parent
+				if (parent[idField] == parentId)
+				{
+					// create children array if not done yet
+					if (!parent._children) parent._children = [];
+
+					// if child is not included in children array, append the child
+					if (!parent._children.includes(child)) parent._children.push(child);
+
+					// parent found
+					return true;
+				}
+				// search children for parents
+				else if (parent._children) this._appendChild(parent._children, child);
+			}
+
+			// parent not found
+			return false;
+		},
+		onFormClosed(){
+			// Deselect all rows
+			this.$refs.softwareanforderungNachSwTable.tabulator.deselectRow();
+		}
+	},
+	template: `
+<div class="softwareanforderungNachSw overflow-hidden">
+	<div class="row-col d-flex ms-auto">
+		<!-- TODO phrase exists, but not breaking line properly -->
+		<!--<button class="btn btn-secondary text-start ms-auto" @click="">{{ $p.t('global/swNichtGefundenHierBestellen') }}</button>--> 
+		<button class="btn btn-secondary text-start ms-auto" @click="">Software nicht gefunden?<br>Hier bei IT-Services bestellen</button>
+	</div>
+	<div class="row mb-5">
+		<div class="col">
+			<core-filter-cmpt
+				ref="softwareanforderungNachSwTable"
+				uniqueId="softwareanforderungNachSwTable"
+				table-only
+				:side-menu="false"
+				:tabulator-options="tabulatorOptions">
+				<template v-slot:actions>
+					<button class="btn btn-primary" @click="openSoftwareanforderungForm()">{{ $p.t('global/swFuerLvAnfordern') }}</button>
+					<div class="form-check form-check-inline">
+						<input
+							class="form-check-input"
+							type="checkbox"
+							v-model="cbDataTree"
+							:checked="cbDataTree"
+							@change="reloadTabulator">
+						<label class="form-check-label">{{ $p.t('global/hierarchieAnsicht') }}</label>
+					</div>
+					<div class="form-check form-check-inline" v-show="cbDataTree">
+						<input
+							class="form-check-input"
+							type="checkbox"
+							v-model="cbDataTreeStartExpanded"
+							:checked="cbDataTreeStartExpanded"
+							@change="reloadTabulator">
+						<label class="form-check-label">{{ $p.t('global/aufgeklappt') }}</label>
+					</div>
+				</template>
+			</core-filter-cmpt>						
+		</div>
+	</div>
+	
+	<!-- Form -->
+	<softwareanforderung-form ref="softwareanforderungForm" @form-closed="onFormClosed"></softwareanforderung-form>
+	
+	<!-- Software Raumverfügbarkeit  -->
+	<div class="offcanvas offcanvas-start w-50" tabindex="-1" id="softwarelisteOffcanvas">
+		<div class="offcanvas-header">
+			<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+		</div>
+		<raumzuordnung ref="raumzuordnung"></raumzuordnung>
+	</div>
+</div>
+`
+};
