6303ff11e3ad540dfe4ead63b1128e398547e6f8
Cris <hainberg@technikum-wien.at>
2024-07-10 14:41:54 +0200

    Made 'SW nicht gefunden'-Button responsive

--- public/js/components/Softwareanforderung/SoftwareanforderungNachSw.js
+++ public/js/components/Softwareanforderung/SoftwareanforderungNachSw.js
@@ -194,8 +194,8 @@ export default {
 	template: `
 <div class="softwareanforderungNachSw overflow-hidden">
 	<div class="row d-flex my-3 align-items-center">
-		<div class="col-10 h4">{{ $p.t('global/swAnforderungUeberAuswahlVonSw') }}</div>
-		<div class="col-2 d-flex justify-content-end">
+		<div class="col-sm-9 col-md-10 h4">{{ $p.t('global/swAnforderungUeberAuswahlVonSw') }}</div>
+		<div class="col-sm-3 col-md-2 d-flex justify-content-end">
 		<!-- TODO phrase exists, but not breaking line properly -->
 		<!--<button class="btn btn-secondary text-start ms-auto" @click="">{{ $p.t('global/swNichtGefundenHierBestellen') }}</button>--> 
 			<button class="btn btn-secondary text-start" @click="">Software nicht gefunden?<br>Hier bei IT-Services bestellen</button>
